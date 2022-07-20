9ac52b9843faaeb6257483d01e101217e96b76f8
tunt-2001 <trongtu28032001@gmail.com>
2022-07-20 21:44:44 +0700

    zzzzz

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    introTitle: '... à!',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao anh cứ đi nhầm đường
+    Lạc vào tim em lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: 'hihi',
+    title: 'Phải chăng anh đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là thích em <33',
+    btnNo: 'Không, Cô gái à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao anh lại yêu em <3',
+    btnReply: 'Gửi cho em <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Em biết mà 🥰. Yêu anh nhiều nhiều 😘😘',
+    messDesc: 'công chúa.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'http://facebook.com/profile.php?id=100011090316948' 
+}
